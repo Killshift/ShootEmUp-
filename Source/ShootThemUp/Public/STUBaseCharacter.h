161ab0f87a4580427ae8008acf3315cd00e6aa4b
Killshift <Killshift@yandex.ru>
2021-08-28 12:38:32 +0300

    43. Lection. Weapon Spawning and attaching to Character

--- Source/ShootThemUp/Public/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/STUBaseCharacter.h
@@ -10,6 +10,7 @@ class UCameraComponent;
 class USpringArmComponent;
 class USTUHealthComponent;
 class UTextRenderComponent;
+class ASTUBaseWeapon;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
@@ -59,6 +60,9 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Damage")
 	FVector2D LandedDamage = FVector2D(10.0f, 100.0f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+	TSubclassOf<ASTUBaseWeapon> WeaponClass;
 private:
 
 	void MoveForward(float Amount);
@@ -73,4 +77,6 @@ private:
 
 	UFUNCTION()
 	void OnGroundLanded(const FHitResult& Hit);
+
+	void SpawnWeapon();
 };
---
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -0,0 +1,28 @@
+// Test Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBaseWeapon.generated.h"
+
+class WeaponMesh;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBaseWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+		ASTUBaseWeapon();
+
+protected:
+		virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	USkeletalMeshComponent* WeaponMesh;
+
+public:	
+
+
+};
