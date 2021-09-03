c2b532d45064409b46d7fc2c5d0c7f2355fb0983
Killshift <Killshift@yandex.ru>
2021-09-03 10:17:21 +0300

    54. SpawnProjectile

--- Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STULauncherWeapon.h
@@ -1,4 +1,4 @@
-// Test Project
+// Shoot Them Up Game, All Rights Reserved.
 
 #pragma once
 
@@ -6,12 +6,19 @@
 #include "Weapon/STUBaseWeapon.h"
 #include "STULauncherWeapon.generated.h"
 
-/**
- * 
- */
+class ASTUProjectile;
+
 UCLASS()
 class SHOOTTHEMUP_API ASTULauncherWeapon : public ASTUBaseWeapon
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    virtual void StartFire() override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+    TSubclassOf<ASTUProjectile> ProjectileClass;
+
+    virtual void MakeShot() override;
 };
