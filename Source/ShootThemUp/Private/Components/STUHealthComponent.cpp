0621afff565ae46394292ecf5cac5531ced8e924
Killshift <Killshift@yandex.ru>
2021-10-01 11:54:49 +0300

    67. Lection - HP bar widget implementation (WBP version)

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -1,64 +1,62 @@
-// Test Project
-
+// Shoot Them Up Game, All Rights Reserved.
 
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
 #include "Engine/World.h"
 #include "TimerManager.h"
 
-DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
+DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All);
 
 USTUHealthComponent::USTUHealthComponent()
 {
-
-	PrimaryComponentTick.bCanEverTick = true;
-	
+    PrimaryComponentTick.bCanEverTick = false;
 }
-
 
 void USTUHealthComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 
-	SetHealth(MaxHealth);
+    check(MaxHealth > 0);
 
-	AActor* ComponentOwner = GetOwner();
-	if(ComponentOwner)
-	{
-		ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAnyDamage);
-	}
-	
+    SetHealth(MaxHealth);
+
+    AActor* ComponentOwner = GetOwner();
+    if (ComponentOwner)
+    {
+        ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAnyDamage);
+    }
 }
 
-void USTUHealthComponent::OnTakeAnyDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+void USTUHealthComponent::OnTakeAnyDamage(
+    AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
-	if( Damage <= 0 || IsDead()  || !GetWorld() ) {return;}
+    if (Damage <= 0.0f || IsDead() || !GetWorld()) return;
 
-	SetHealth(Health - Damage);
-	GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+    SetHealth(Health - Damage);
+    GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
 
-	if(IsDead())
-	{
-		OnDeath.Broadcast();
-	}
-	else if(AutoHeal)
-	{
-		GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USTUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
-	}
+    if (IsDead())
+    {
+        OnDeath.Broadcast();
+    }
+    else if (AutoHeal)
+    {
+        GetWorld()->GetTimerManager().SetTimer(HealTimerHandle, this, &USTUHealthComponent::HealUpdate, HealUpdateTime, true, HealDelay);
+    }
 }
 
 void USTUHealthComponent::HealUpdate()
 {
-	SetHealth(Health + HealModifier);
+    SetHealth(Health + HealModifier);
 
-	if( FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld() )
-	{
-		GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
-	}
+    if (FMath::IsNearlyEqual(Health, MaxHealth) && GetWorld())
+    {
+        GetWorld()->GetTimerManager().ClearTimer(HealTimerHandle);
+    }
 }
 
 void USTUHealthComponent::SetHealth(float NewHealth)
 {
-	Health = FMath::Clamp(NewHealth, 0.0f , MaxHealth);
-	OnHealthChanged.Broadcast(Health);
+    Health = FMath::Clamp(NewHealth, 0.0f, MaxHealth);
+    OnHealthChanged.Broadcast(Health);
 }
