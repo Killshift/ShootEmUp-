94548d83a0aedf78b8334d409fc926569a611c5a
Killshift <Killshift@yandex.ru>
2021-10-06 18:52:32 +0300

    75. Lection - Respawning of Pickups(timer + disabling/enabling collision)

--- Source/ShootThemUp/Public/Pickups/STUAmmoPickup.h
+++ Source/ShootThemUp/Public/Pickups/STUAmmoPickup.h
@@ -1,4 +1,4 @@
-// Test Project
+// Shoot Them Up Game, All Rights Reserved.
 
 #pragma once
 
@@ -6,12 +6,11 @@
 #include "Pickups/STUBasePickup.h"
 #include "STUAmmoPickup.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class SHOOTTHEMUP_API ASTUAmmoPickup : public ASTUBasePickup
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+private:
+    virtual bool GivePickupTo(APawn* PlayerPawn) override;
 };
