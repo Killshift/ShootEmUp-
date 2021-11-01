08ad19b4175249f85981a9b75d4be4ad0748d427
Killshift <Killshift@yandex.ru>
2021-11-01 16:55:38 +0300

    100. Lection -  AI Changing empty weapon

--- Source/ShootThemUp/Public/Components/STUWeaponComponent.h
+++ Source/ShootThemUp/Public/Components/STUWeaponComponent.h
@@ -17,9 +17,9 @@ class SHOOTTHEMUP_API USTUWeaponComponent : public UActorComponent
 public:
     USTUWeaponComponent();
 
-    void StartFire();
+    virtual void StartFire();
     void StopFire();
-    void NextWeapon();
+    virtual void NextWeapon();
     void Reload();
 
     bool GetCurrentWeaponUIData(FWeaponUIData& UIData) const;
@@ -29,46 +29,48 @@ public:
 
 protected:
     UPROPERTY(EditDefaultsOnly, Category = "Weapon")
-        TArray<FWeaponData> WeaponData;
+    TArray<FWeaponData> WeaponData;
 
     UPROPERTY(EditDefaultsOnly, Category = "Weapon")
-        FName WeaponEquipSocketName = "WeaponSocket";
+    FName WeaponEquipSocketName = "WeaponSocket";
 
     UPROPERTY(EditDefaultsOnly, Category = "Weapon")
-        FName WeaponArmorySocketName = "ArmorySocket";
+    FName WeaponArmorySocketName = "ArmorySocket";
 
     UPROPERTY(EditDefaultsOnly, Category = "Animation")
-        UAnimMontage* EquipAnimMontage;
+    UAnimMontage* EquipAnimMontage;
+
+    UPROPERTY()
+    ASTUBaseWeapon* CurrentWeapon = nullptr;
+
+    UPROPERTY()
+    TArray<ASTUBaseWeapon*> Weapons;
+
+    int32 CurrentWeaponIndex = 0;
 
     virtual void BeginPlay() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+    bool CanFire() const;
+    bool CanEquip() const;
+    void EquipWeapon(int32 WeaponIndex);
+
 private:
     UPROPERTY()
-        ASTUBaseWeapon* CurrentWeapon = nullptr;
+    UAnimMontage* CurrentReloadAnimMontage = nullptr;
 
-    UPROPERTY()
-        TArray<ASTUBaseWeapon*> Weapons;
-
-    UPROPERTY()
-        UAnimMontage* CurrentReloadAnimMontage = nullptr;
-
-    int32 CurrentWeaponIndex = 0;
     bool EquipAnimInProgress = false;
     bool ReloadAnimInProgress = false;
 
     void InitAnimations();
     void SpawnWeapons();
     void AttachWeaponToSocket(ASTUBaseWeapon* Weapon, USceneComponent* SceneComponent, const FName& SocketName);
-    void EquipWeapon(int32 WeaponIndex);
 
     void PlayAnimMontage(UAnimMontage* Animation);
 
     void OnEquipFinished(USkeletalMeshComponent* MeshComp);
     void OnReloadFinished(USkeletalMeshComponent* MeshComp);
 
-    bool CanFire() const;
-    bool CanEquip() const;
     bool CanReload() const;
 
     void OnClipEmpty(ASTUBaseWeapon* AmmoEmptyWeapon);
